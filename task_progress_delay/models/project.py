1dc9aa33bea4e639c9dd03fa02b64d3703366a48
tascodoo <87600969+tascodoo@users.noreply.github.com>
2021-08-31 18:22:11 +0530

    Update project.py

--- task_progress_delay/models/project.py
+++ task_progress_delay/models/project.py
@@ -299,7 +299,7 @@ class ProjectTask(models.Model):
         if vals.get('stage_id', False):
             total_time = 0
             if len(self.timesheet_ids) == 0:
-                # raise Warning(_('"Please Fill the task cost or Government fee"'))
+                raise Warning(_('"Please Fill the task cost or Government fee"'))
                 print('removeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeed as its not working')
             for j in self.timesheet_ids:
                 if j.cost_stage + j.gov_fee < 0:
